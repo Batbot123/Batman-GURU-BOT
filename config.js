4f16041c8df317dda3a58149daa94b6b3dea36f3
Batbot123 <133778823+Batbot123@users.noreply.github.com>
2023-05-16 22:05:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94717259720', 'Owner', true],
+  ['94717259720', 'Batbot', true], 
   [''] 
 ] //Number of owners
 
